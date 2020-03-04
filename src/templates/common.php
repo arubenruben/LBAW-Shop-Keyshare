4ca6c064eb07bc4e2dff76ad67805283ab873cc4
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-04 18:05:13 +0000

    Martim page translation to PHP

--- src/templates/common.php
+++ src/templates/common.php
@@ -53,38 +53,90 @@ function drawHeader($type)
 
     ?>
 
-    <?php }
+        <?php }
 
-function drawNavbar($type)
-{
-    switch ($type) {
-            //Draw Homepage navbar
-        case 0:
+    function drawNavbar($type)
+    {
+        switch ($type) {
+                //Draw Homepage navbar
+            case 0: ?>
+
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                    <div class="container">
+                        <a class="navbar-brand" href="#">Start Bootstrap</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarResponsive">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home
+                                        <span class="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
 
 
-
-            break;
-
-
+        <?php
+                break;
 
 
-        default:
+            default:
 
-            break;
-    }
+                break;
+        }
 
-    ?>
+        ?>
 
 
 
 
     <?php
-}
+    }
 
 function drawFooter()
-{ ?>
+    { ?>
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+                <br>
+                <hr class="style1"> <br>
+                <div class="row ">
+                    <div class="col-md-6">
+                        <h5 class="title"> More </h5>
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#"> Contact </a>
+                            </li>
+                            <li>
+                                <a href="#"> FAQs </a>
+                            </li>
+                            <li>
+                                <a href="#"> About us </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-6 d-flex justify-content-end align-items-end">
+                        <p>© Copyright 2020 Key Share. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </body>
 
     </html>
 <?php
-} ?>
+    } ?>
