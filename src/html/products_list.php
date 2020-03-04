eeeca1a5b990b6b9ab57de1fcb2b33cfbaf44b71
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-04 21:58:00 +0000

    Initial schaffold right aligns missing

---
+++ src/html/products_list.php
@@ -0,0 +1,236 @@
+<?php
+include_once('../templates/common.php');
+
+drawHead();
+//drawHeader(0);
+drawNavbar(0);
+?>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+            <div class="sidebar-sticky">
+                <div class="list-group">
+                    <div>
+                        <header>Categories</header>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">Button with data-target</button>
+                        <div class="collapse" id="collapseExample2">
+                            <div class="form-check-inline">
+                                <label class="form-check-label">
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>Action</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox1" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>Sports</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox1" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>Racing</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox1" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>Adventure</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox1" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <header>Platforms</header>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample">Button with data-target</button>
+                        <div class="collapse" id="collapseExample1">
+                            <div class="form-check-inline">
+                                <label class="form-check-label">
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>PC</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox2" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>PS4</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox2" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>Xbox</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox2" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <header>Genres</header>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Button with data-target</button>
+                        <div class="collapse" id="collapseExample">
+                            <div class="form-check-inline">
+                                <label class="form-check-label">
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>Game</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox1" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                    <div class="checkbox">
+                                        <label for="checkbox1">
+                                            <span>DLC</span>
+                                        </label>
+                                        <input type="checkbox" id="checkbox1" class="checkbox style-2 pull-right" checked="checked" />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container col-10">
+            <!--Order By-->
+            <div class="row">
+                <div class="col-2">
+
+                    <span>Order By</span>
+                    <select class="form-control form-control-lg">
+                        <option value="" selected disabled hidden>Choose here</option>
+                        <option>Price</option>
+                        <option>Rating</option>
+                        <option>Release Date</option>
+                    </select>
+                </div>
+
+
+            </div>
+            <div class="row">
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+
+                <div class="col card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Item One</a>
+                        </h4>
+                        <h5>$24.99</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<?php drawFooter(); ?>
--- src/templates/common.php
+++ src/templates/common.php
@@ -13,6 +13,9 @@ function drawHead($jsArray = null)
 
         <!-- bootstrap -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <!-- fontawesome -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
         <!--font Sans-Serif-->
@@ -61,30 +64,35 @@ function drawHeader($type)
                 //Draw Homepage navbar
             case 0: ?>
 
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                    <div class="container">
-                        <a class="navbar-brand" href="#">Start Bootstrap</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarResponsive">
-                            <ul class="navbar-nav ml-auto">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#">Home
-                                        <span class="sr-only">(current)</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Services</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
 
@@ -106,9 +114,9 @@ function drawHeader($type)
     <?php
     }
 
-function drawFooter()
+    function drawFooter()
     { ?>
-        <!-- Footer -->
+        <!-- Footer 
         <footer>
             <div class="container">
                 <br>
@@ -135,6 +143,8 @@ function drawFooter()
                 </div>
             </div>
         </footer>
+
+    -->
     </body>
 
     </html>
