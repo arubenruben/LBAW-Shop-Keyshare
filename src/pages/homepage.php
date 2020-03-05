96bd51400a30b7ffce2cda8d7a9523a5e8756b07
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2020-03-05 21:52:59 +0000

    merge

---
+++ src/pages/homepage.php
@@ -0,0 +1,10 @@
+<?php
+	//includes
+	include_once('../templates/tpl_common.php');
+	include_once('../templates/tpl_homepage.php');
+	//page
+	drawHead();
+	drawHeader(0);
+	drawHomepage();
+	drawFooter(); 
+?>
---
+++ src/templates/tpl_homepage.php
@@ -0,0 +1,155 @@
+<?php function drawHomepage() { ?>
+	<div class="container">
+		<div class="col">
+			<div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+				<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+				<div class="carousel-item active">
+					<img class="d-block img-fluid" src="images/1.jpg" alt="First slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block img-fluid" src="images/2.jpg" alt="Second slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block img-fluid" src="images/3.png" alt="Third slide">
+				</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+				</a>
+			</div>
+			
+
+			<div class="row">
+				<div class="col-sm-9">
+					<h5 class="title"> Most popular </h5>
+				</div>
+				<div class="col-sm-2">
+					<button type="button" class="btn btn-outline-dark btn-lg"><i class="fas fa-angle-left"></i></button>
+					<button type="button" class="btn btn-outline-dark btn-lg"><i class="fas fa-angle-right"></i></button>
+				</div>	
+			</div>
+			<div id="most-popular" class="row mt-2">
+					<div class="col-sm-2">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+							<div class="card-body">
+								<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+								<h5>$24.99</h5>
+							</div>
+						</div>
+					</div>
+	
+					<div class="col-sm-2">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+							<div class="card-body">
+								<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+								<h5>$24.99</h5>
+							</div>
+						</div>
+					</div>
+	
+					<div class="col-sm-2">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+							<div class="card-body">
+								<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+								<h5>$24.99</h5>
+							</div>
+						</div>
+					</div>
+	
+					<div class="col-sm-2">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+							<div class="card-body">
+								<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+								<h5>$24.99</h5>
+							</div>
+						</div>
+					</div>
+	
+					<div class="col-sm-2">
+						<div class="card h-100">
+							<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+							<div class="card-body">
+								<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+								<h5>$24.99</h5>
+							</div>
+						</div>
+					</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-9">
+					<h5 class="title"> Most recent </h5>
+				</div>
+				<div class="col-sm-2">
+					<button type="button" class="btn btn-outline-dark btn-lg"><i class="fas fa-angle-left"></i></button>
+					<button type="button" class="btn btn-outline-dark btn-lg"><i class="fas fa-angle-right"></i></button>
+				</div>	
+			</div>
+			<div id="most-recent" class="row mt-2">
+				<div class="col-sm-2">
+					<div class="card h-100">
+						<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+						<div class="card-body">
+							<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+							<h5>$24.99</h5>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-2">
+					<div class="card h-100">
+						<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+						<div class="card-body">
+							<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+							<h5>$24.99</h5>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-2">
+					<div class="card h-100">
+						<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+						<div class="card-body">
+							<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+							<h5>$24.99</h5>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-2">
+					<div class="card h-100">
+						<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+						<div class="card-body">
+							<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+							<h5>$24.99</h5>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-2">
+					<div class="card h-100">
+						<a href="#"><img class="card-img-top" src="../../assets/images/fifa19.JPG" alt=""></a>
+						<div class="card-body">
+							<h6 class="card-title"> <a href="#"> Star Wars Jedi: Fallen Order </a> </h6>
+							<h5>$24.99</h5>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php } ?>
