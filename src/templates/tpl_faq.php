082fed7e3b58ccbf8b25285978c1662d87cfd5e7
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-03-08 14:18:10 +0000

    font fix

--- src/templates/tpl_faq.php
+++ src/templates/tpl_faq.php
@@ -1,7 +1,7 @@
 <?php function drawFAQ(){ ?>
     <div id="container" class="container mt-5">
         <div class="row text-center pt-5 md-12">
-            <div class="col">
+            <div class="col mb-5">
                 <h1> KeyShare FAQ</h1>
             </div>
             <div class="col-10 mx-auto">
