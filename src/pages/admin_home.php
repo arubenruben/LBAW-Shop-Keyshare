1007feb7835335eecfec3eb0cdb3cbe43df4752c
luis ramos <deco.luis@live.com.pt>
2020-03-08 14:02:09 +0000

    added admin

---
+++ src/pages/admin_home.php
@@ -0,0 +1,10 @@
+<?php
+	//includes
+	include_once('../templates/tpl_common.php');
+	include_once('../templates/tpl_admin.php');
+	//page
+	drawHead();
+	drawHeader(0);
+	drawAdminInterface();
+	drawFooter(); 
+?>
---
+++ src/templates/tpl_admin.php
@@ -0,0 +1,32 @@
+<?php 
+function drawAdminInterface() { ?>
+<section class="aboutus py-5" id="aboutus">
+    <div class="container-fluid">
+        <div class="column">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="">
+                        <h4 class="pl-3 py-2">Categories</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="dashboard.html" class="list-group-item  bg-active"><i class="fa fa-tachometer"></i> Home </a> </li>
+                            <li><a href="my-profile.html" class="list-group-item"> Products </a> </li>
+                            <li><a href="my-listings.html" class="list-group-item"> Categories </a> </li>
+                            <li><a href="my-enquiry.html" class="list-group-item"> Genres </a> </li>
+                            <li><a href="add-business.html" class="list-group-item"> Platforms </a> </li>
+                            <li><a href="login.html" class="list-group-item"> Users </a> </li>
+                            <li><a href="signup.html" class="list-group-item"> Reports </a> </li>
+                            <li><a href="account-settings.html" class="list-group-item"> Transactions </a> </li>
+                            <li><a href="change-password.html" class="list-group-item"> Reviews </a> </li>
+                            <li><a href="listings-page.html" class="list-group-item"> FAQ </a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php } 
+
+function drawAdminHomePage() { ?>
+
+<?php } ?>
