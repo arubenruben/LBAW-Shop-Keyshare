82bd4cde58ef5c3cb527bfa851f10be2c92685c6
luis ramos <deco.luis@live.com.pt>
2020-03-08 17:06:36 +0000

    product with feedback popup done

--- src/pages/feedback.php
+++ src/pages/feedback.php
@@ -1,10 +1,10 @@
 <?php
 	//includes
 	include_once('../templates/tpl_common.php');
-	include_once('../templates/tpl_product.php');
+	include_once('../templates/tpl_feedback.php');
 	//page
 	drawHead();
 	// drawHeader(0);
-	drawProduct();
+	drawFeedbackPopup();
 	drawFooter();
 ?>
