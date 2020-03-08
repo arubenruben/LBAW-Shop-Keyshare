4cb36ce1a962fe0bb047a1355aa31f2b0f7592a7
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-08 17:23:26 +0000

    header + footer 100% completed and resposible without gaps

--- src/templates/tpl_common.php
+++ src/templates/tpl_common.php
@@ -35,135 +35,136 @@
     <?php
 } ?>
 
-<!-- header -->
-<?php function drawHeader($type)
-{
-    switch ($type) {
-        case 0: ?>
-            <header class="navbar fixed-top">
-                <div class="col-md-2 col-3 col-xl-1 mt-auto mb-auto">
-                    <a href="homepage.php">
-                        <img class="img-fluid logo" src="../../assets/images/logo/logo.png" />
-                    </a>
-                </div>
-                <div class="col-md-5 col-6 col-xl-6 mt-auto mb-auto">
-                    <form class="form-inline">
-                        <div class="form-group mb-auto">
-                            <i id="headerSearchIcon" class="fas fa-search d-none d-sm-block mr-2"></i>
-                            <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+    <!-- header -->
+    <?php function drawHeader($type)
+    {
+        switch ($type) {
+            case 0: ?>
+                <header class="navbar fixed-top">
+                    <div class="col-md-2 col-3 col-xl-1 mt-auto mb-auto">
+                        <a href="homepage.php">
+                            <img class="img-fluid logo" src="../../assets/images/logo/logo.png" />
+                        </a>
+                    </div>
+                    <div class="col-md-5 col-6 col-xl-6 mt-auto mb-auto">
+                        <form class="form-inline">
+                            <div class="form-group mb-auto">
+                                <i id="headerSearchIcon" class="fas fa-search d-none d-sm-block mr-2"></i>
+                                <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-3 d-none d-md-block">
+                        <div class="row">
+                            <button id="headerExploreButton" type="button" class="btn btn-primary ml-auto mr-2 pl-3 pr-3 d-none d-lg-block">Explore</button>
+                            <button id="headerSellButton" type="button" class="btn btn-primary ml-2 pl-3 pr-3">Sell Now</button>
                         </div>
-                    </form>
-                </div>
-                <div class="col-md-3 d-none d-md-block">
-                    <div class="row">
-                        <button id="headerExploreButton" type="button" class="btn btn-primary ml-auto mr-2 pl-3 pr-3 d-none d-lg-block">Explore</button>
-                        <button id="headerSellButton" type="button" class="btn btn-primary ml-2 pl-3 pr-3">Sell Now</button>
                     </div>
-                </div>
-                <div class="col-md-2 col-1 d-none d-md-block">
-                    <div class="row">
-                        <i id="myAccountIcon" class="fas fa-user headerIcon ml-auto mr-5" data-toggle="modal" data-target=".bs-modal-sm"></i>
-                        <a href="cart.php"><i class="fas fa-shopping-cart headerIcon m-auto cl-orange"></i></a>
-                    </div>
-                </div>
-                <!-- mobile -->
-                <div class="col-2 d-md-none d-xs-block">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="pos-f-t">
-                    <div class="collapse" id="navbarToggleExternalContent">
-                        <div class="ml-auto p-2">
-                            <i id="myAccountIcon" class="fas fa-user headerIcon"></i>
+                    <div class="col-md-2 col-1 d-none d-md-block">
+                        <div class="row">
+                            <i id="myAccountIcon" class="fas fa-user headerIcon ml-auto mr-5" data-toggle="modal" data-target=".bs-modal-sm"></i>
                             <a href="cart.php"><i class="fas fa-shopping-cart headerIcon m-auto cl-orange"></i></a>
                         </div>
                     </div>
-                </div>
+                    <!-- mobile -->
+                    <div class="col-2 d-md-none d-xs-block">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div class="pos-f-t">
+                        <div class="collapse" id="navbarToggleExternalContent">
+                            <div class="ml-auto p-2">
+                                <i id="myAccountIcon" class="fas fa-user headerIcon"></i>
+                                <a href="cart.php"><i class="fas fa-shopping-cart headerIcon m-auto cl-orange"></i></a>
+                            </div>
+                        </div>
+                    </div>
 
-                <?php drawAuthenticationPopup(); ?>
-            </header>
-            <div id="wrapper">
+                    <?php drawAuthenticationPopup(); ?>
+                </header>
+                <div id="wrapper">
+                <?php
+                break;
+            default: ?>
+
+
             <?php
-            break;
-        default: ?>
 
+
+                break;
+        }
+            ?>
 
         <?php
+    } ?>
 
-
-            break;
-    }
-        ?>
-
-    <?php
-} ?>
-
-<!-- navbar -->
-<?php function drawNavbar($type)
-{
-    switch ($type) {
-            //Draw Homepage navbar
-        case 0: ?>
+        <!-- navbar -->
+        <?php function drawNavbar($type)
+        {
+            switch ($type) {
+                    //Draw Homepage navbar
+                case 0: ?>
 
 
 
-    <?php
-            break;
+            <?php
+                    break;
 
 
-        default:
+                default:
 
-            break;
-    }
+                    break;
+            }
 
-    ?>
-<?php } ?>
+            ?>
+        <?php } ?>
 
-<!-- footer -->
-<?php function drawFooter()
-{ ?>
-        <!-- Footer -->
-        <footer id="footerGeneric" class="mt-5">
-            <div class="container mt-2">
-                <hr>
-                <div class="row">
-                    <div class="col">
-                        <h5 class="title"> More </h5>
-                        <ul class="list-unstyled">
-                            <li>
-                                <a href="faq.php"> Help </a>
-                            </li>
-                            <li>
-                                <a href="contact.php"> Contact </a>
-                            </li>
-                            <li>
-                                <a href="about.php"> About us </a>
-                            </li>
-                        </ul>
+        <!-- footer -->
+        <?php function drawFooter()
+        { ?>
+            <!-- Footer -->
+            <footer id="footerGeneric" class="mt-5">
+                <div class="container mt-2">
+                    <hr>
+                    <div class="row">
+                        <div class="col">
+                            <h5 class="title"> More </h5>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="faq.php"> Help </a>
+                                </li>
+                                <li>
+                                    <a href="contact.php"> Contact </a>
+                                </li>
+                                <li>
+                                    <a href="about.php"> About us </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h5 class="title"> Shortcuts </h5>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="about.php"> Profile </a>
+                                </li>
+                                <li>
+                                    <a href="help.php"> Homepage </a>
+                                </li>
+                                <li>
+                                    <a href="contact.php"> All products </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col ml-auto mt-auto mb-auto">
+                            <p>© Copyright 2020 Key Share. All rights reserved.</p>
+                        </div>
                     </div>
-                    <div class="col">
-                        <h5 class="title"> Shortcuts </h5>
-                        <ul class="list-unstyled">
-                            <li>
-                                <a href="about.php"> Profile </a>
-                            </li>
-                            <li>
-                                <a href="help.php"> Homepage </a>
-                            </li>
-                            <li>
-                                <a href="contact.php"> All products </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col justify-content-end align-items-end d-none d-md-flex d-lg-flex">
-                        <p>© Copyright 2020 Key Share. All rights reserved.</p>
-                    </div>
+            </footer>
+            <!--This Div closes the container that mantains the footer at the bottom -->
                 </div>
-        </footer>
-        <!--This Div closes the container that mantains the footer at the bottom -->
-    </div>
     </body>
+
     </html>
 <?php } ?>
 
