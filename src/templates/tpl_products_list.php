5eb2452694c07dd2de39fa0fb7f92cdbd4032377
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-03-10 15:32:44 +0000

    products list update

--- src/templates/tpl_products_list.php
+++ src/templates/tpl_products_list.php
@@ -3,6 +3,9 @@
         <div class="row mt-5">
             <?php productListingSideBar(); ?>
             <div class="col ml-auto mr-auto">
+                <div class="row"> 
+                    <div class="col text-left"> </div>
+</div>
                 <!--Filters -->
                 <div class="row ml-1 mr-1 d-none">
                     <div class="sideBarButton">
@@ -104,7 +107,9 @@
                 </div>
             </div>
         </div>
+        <?php drawFilterSideBar(); ?>
     </div>
+
 <?php } ?>
 
 <?php function productListingSideBar() { ?>
@@ -229,3 +234,30 @@
     </div>
 
 <?php } ?>
+
+<?php function drawFilterSideBar(){ ?>
+   
+    <!-- Modal -->
+	<div class="modal left fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Left Sidebar</h4>
+				</div>
+
+				<div class="modal-body">
+					<p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					</p>
+				</div>
+
+			</div><!-- modal-content -->
+		</div><!-- modal-dialog -->
+	</div><!-- modal -->
+	
+
+
+
+<?php }
+?>
