a468ce27fec593abbd112fd1f22b4fc069a93167
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-03-10 09:15:49 +0000

    cart updated

--- src/pages/cart.php
+++ src/pages/cart.php
@@ -2,6 +2,7 @@
 	//includes
 	include_once('../templates/tpl_common.php');
 	include_once('../templates/tpl_cart.php');
+	include_once("../templates/tpl_feedback.php");
 	//page
 	drawHead();
 	drawHeader(0);
