44167a85ab3d967c7ada76416d289ddf27d81711
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-11 10:18:12 +0000

    Offer page com o espaco branco que o ze nao gostava resolvido

--- src/templates/tpl_offer.php
+++ src/templates/tpl_offer.php
@@ -9,7 +9,7 @@ function drawOffer()
                 <h3>Choose Game</h3>
                 <div class="row">
                     <div class="col-5 mt-3">
-                        <img class="img-fluid productPageImgPreview" src="../../assets/images/games/GTAV/1.png"/>
+                        <img class="img-fluid productPageImgPreview" src="../../assets/images/games/GTAV/1.png" />
                     </div>
                     <div class="col-7 mt-2">
                         <form action="#" method="GET">
@@ -48,45 +48,55 @@ function drawOffer()
                     </div>
                 </div>
                 <hr>
-                <div class="row">
-                    <div class="col">
-                        <h3>Pricing</h3>
-                        <div class="form-inline mt-3 ml-4">
-                            <label for="inputGamePrice">
-                                <h5>Price</h5>
-                                <input type="text" class="form-control mt-auto mb-auto ml-3" id="inputGamePrice" placeholder="Price" value="10€">
-                            </label>
+                <div class="col">
+                    <h3 class="d-none d-lg-block">Pricing</h3>
+                    <div class="row">
+                        <div class="col mt-auto mb-auto">
+                            <div class="row">
+                                <div class="form-inline mt-auto mb-auto ml-5 mr-auto">
+                                    <label for="inputGamePrice">
+                                        <h5 class="mr-4">Price</h5>
+                                        <input type="text" class="form-control" id="inputGamePrice" placeholder="Price" value="10€">
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                        <table class="table table-responsive mt-4 text-center">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Sale Nr</th>
-                                    <th scope="col">Start Date</th>
-                                    <th scope="col">End Date</th>
-                                    <th scope="col">Percentage</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>2020/03/15</td>
-                                    <td>2020/03/20</td>
-                                    <td>40%</td>
-                                    <td><i class="fas fa-times-circle"></i></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>2020/04/15</td>
-                                    <td>2020/05/20</td>
-                                    <td>30%</td>
-                                    <td><i class="fas fa-times-circle"></i></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <div id="offerAddNewSaleContainer" class="row mt-4">
-                            <i class="fas fa-plus-circle ml-auto mb-auto mt-auto"></i>
-                            <span class="mt-auto mt-auto mb-auto mr-auto ml-3">Add New Sale</span>
+                        <div class="col">
+                            <div class="row">
+                                <table class="table table-responsive mt-2 text-center">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Sale Nr</th>
+                                            <th scope="col">Start Date</th>
+                                            <th scope="col">End Date</th>
+                                            <th scope="col">Percentage</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>2020/03/15</td>
+                                            <td>2020/03/20</td>
+                                            <td>40%</td>
+                                            <td><i class="fas fa-times-circle"></i></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>2020/04/15</td>
+                                            <td>2020/05/20</td>
+                                            <td>30%</td>
+                                            <td><i class="fas fa-times-circle"></i></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="row">
+                                <div id="offerAddNewSaleContainer" class="row ml-auto mr-auto">
+                                    <i class="fas fa-plus-circle mb-auto mt-auto"></i>
+                                    <span class="mt-auto mt-auto mb-auto mr-auto ml-3">Add New Sale</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
