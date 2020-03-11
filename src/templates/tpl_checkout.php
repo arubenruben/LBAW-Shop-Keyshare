070ba4c927f831641e7a8392982fe2f1b00ed67e
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2020-03-11 23:57:31 +0000

    checkout 3 homepage

--- src/templates/tpl_checkout.php
+++ src/templates/tpl_checkout.php
@@ -105,7 +105,7 @@ function drawCheckoutThirdPage($sucess = true)
                             <h3 id="checkoutStatusTitle">Fail</h3>
                         <?php
                         }  ?>
-                        <a href="user.php" id="checkoutStatusButton" class="btn btn-primary btn-lg mt-3">Back to My Account</a>
+                        <a href="homepage.php" id="checkoutStatusButton" class="btn btn-primary btn-lg mt-3">Back to Homepage</a>
                     </div>
                 </div>
             </div>
