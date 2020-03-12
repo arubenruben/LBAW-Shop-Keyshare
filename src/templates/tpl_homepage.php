477094ad9726f58ed21e0922df1ef7a0ec226b99
Ruben Almeida <almeidaruben35@gmail.com>
2020-03-12 00:46:12 +0000

    Fifa wallpapper

--- src/templates/tpl_homepage.php
+++ src/templates/tpl_homepage.php
@@ -11,13 +11,13 @@
 				</ol>
 				<div class="carousel-inner">
 					<div class="carousel-item active">
-						<a href="product.php"><img src="../../assets/images/games/carousel/1.png" class="d-block w-100"></a>
+						<a href="product.php"><img src="../../assets/images/games/carousel/1.jpg" class="d-block w-100"></a>
 					</div>
 					<div class="carousel-item">
-						<a href="product.php"><img src="../../assets/images/games/carousel/1.png" class="d-block w-100"></a>
+						<a href="product.php"><img src="../../assets/images/games/carousel/1.jpg" class="d-block w-100"></a>
 					</div>
 					<div class="carousel-item">
-						<a href="product.php"><img src="../../assets/images/games/carousel/1.png" class="d-block w-100"></a>
+						<a href="product.php"><img src="../../assets/images/games/carousel/1.jpg" class="d-block w-100"></a>
 					</div>
 				</div>
 				<a id="carouselArrowLeft" class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
