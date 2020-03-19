731aa3c55e141c915ec3e60ac873705830ccb409
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2020-03-19 15:47:19 +0000

    banned in user reports

--- src/templates/tpl_user.php
+++ src/templates/tpl_user.php
@@ -962,6 +962,13 @@
     <div id="content" class="container mt-5">
         <div class="row mt-5">
             <div class="col-sm-12 usercontent-left">
+                <?php if($type == "banned"){?>    
+                    <div class="row mb-2">
+                        <div class="col-7 hoverable color:red text-center mx-auto alert alert-danger" role="alert" data-toggle="modal" data-target="#modalAppeal">
+                            You are currently banned! Some functionalities are disabled. <strong>Click to appeal</strong>
+                        </div>
+                    </div>
+                <?php }?>
                 <div class="row px-3">
                     <div class="col-sm-9 " style=" display:flex; align-items: center;">
                         <h4 class="text-left">My Reports<span class="badge ml-1 badge-secondary">1</span></h4>
