97fb6d5de51ced657265c5f1a51d8746203ea425
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-04-07 19:14:41 +0100

    deleted attribute on offer

--- database/schema.sql
+++ database/schema.sql
@@ -61,6 +61,8 @@ CREATE TABLE regular_user (
   username TEXT NOT NULL UNIQUE,
   email TEXT NOT NULL UNIQUE,
   description TEXT DEFAULT NULL,
+  name_tsvector tsvector DEFAULT NULL,
+  weight_tsvector  tsvector DEFAULT NULL,
   password TEXT NOT NULL,
   rating INTEGER DEFAULT NULL,
   birth_date date NOT NULL,
@@ -83,6 +85,7 @@ CREATE TABLE offer (
   seller INTEGER REFERENCES regular_user(id) ON DELETE SET NULL ON UPDATE CASCADE,
   product INTEGER REFERENCES product(id) ON DELETE SET NULL ON UPDATE CASCADE,
   stock INTEGER NOT NULL DEFAULT 1,
+  deleted BOOLEAN NOT NULL,
     
   CONSTRAINT price_ck CHECK (price > 0),
   CONSTRAINT init_date_ck CHECK (init_date <= NOW()),
