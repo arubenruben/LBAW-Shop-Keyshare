664f475c2fad000cd7c6fd34f3cbc561b81321b7
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-04-07 20:25:03 +0100

    DROP SCHEMA IF EXISTS

--- database/schema.sql
+++ database/schema.sql
@@ -2,7 +2,7 @@
 -- Drop old schmema
 -----------------------------------------
 
-DROP SCHEMA public CASCADE;
+DROP SCHEMA IF EXISTS  public CASCADE;
 CREATE SCHEMA public;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO public;
