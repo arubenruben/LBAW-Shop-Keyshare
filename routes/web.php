229b3d5990f48a02166a329188393fb26aa310e8
luis ramos <deco.luis@live.com.pt>
2020-04-17 12:01:45 +0100

    added user routes

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,6 @@
 */
 
 Route::get('/', 'Auth\LoginController@home');
-// Cards
-Route::get('cards', 'CardController@list');
-Route::get('cards/{id}', 'CardController@show');
 
 // API
 Route::put('api/cards', 'CardController@create');
@@ -24,7 +21,24 @@ Route::post('api/item/{id}', 'ItemController@update');
 Route::delete('api/item/{id}', 'ItemController@delete');
 
 // Authentication
-
 Route::post('login', 'Auth\LoginController@login');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 Route::put('register', 'Auth\RegisterController@register');
+
+// User
+Route::get('user/{id}', 'UserController@show');
+Route::delete('user/{id}', 'UserController@delete');
+Route::get('user/{id}/purchase', 'UserController@showPurchases');
+Route::get('user/{id}/offer', 'UserController@showOffers');
+Route::get('user/{id}/report', 'UserController@showReports');
+Route::post('user/{id}/description', 'UserController@updateDescription');
+Route::post('user/{id}/password', 'UserController@updatePassword');
+Route::post('user/{id}/image', 'UserController@updateImage');
+Route::delete('user/{id}/image', 'UserController@deleteImage');
+Route::post('user/{id}/email', 'UserController@updateEmail');
+Route::post('user/{id}/paypal', 'UserController@updatePayPal');
+
+// Cards
+Route::get('cards', 'CardController@list');
+Route::get('cards/{id}', 'CardController@show');
+
