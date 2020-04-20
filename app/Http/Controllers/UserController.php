c21e672e1f2dc8c89b920c67b7c765a24ec1153d
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-04-20 17:07:14 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,12 +13,20 @@ use App\User;
 class UserController extends Controller
 {
     public function getUser($username){
-        $user_id = DB::table('regular_user')->select('id')->where('username', '=', $username)->first()->id;
-        return User::findOrFail($user_id);
-    }
-    public function show($username) {
+        $user = DB::table('regular_user')->select('id')->where('username', '=', $username)->first();
 
+        if($user != null)
+            return User::findOrFail($user->id);
+
+        else
+            return null;
+    }
+
+    public function show($username) {
         $user = $this->getUser($username);
+
+        if($user == null)
+            return response(404);
 
         try {
             $this->authorize('ownUser', $user->id);
@@ -45,6 +53,9 @@ class UserController extends Controller
     public function showOffers($username) {
         $user = $this->getUser($username);
         $isOwner = true;
+
+        if($user == null)
+            return response(404);
 
         try {
             $this->authorize('edit', $user->id);
