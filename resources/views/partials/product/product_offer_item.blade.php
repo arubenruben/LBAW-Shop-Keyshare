54a4de8941448ffc7667b92206cf4b610d693359
Martim de Carvalho e Sousa Pinto da Silva <martimpintodasilva@gmail.com>
2020-05-09 11:22:04 +0100

    testing feedback

--- resources/views/partials/product/product_offer_item.blade.php
+++ resources/views/partials/product/product_offer_item.blade.php
@@ -1,4 +1,3 @@
-
 <tr class="offer {{  $display ? '' : 'offer_outside'}}">
     <td scope="row" class="border-0 align-middle">
         <div class="p-2 m-0">
@@ -11,21 +10,22 @@
         </div>
     </td>
     @if($offer->price != $offer->discount_price())
-    <td class="text-center align-middle"><del><strong>${{$offer->price}}</strong></del><strong
-            class="cl-green pl-2">${{$offer->discount_price()}}</strong></td>
+        <td class="text-center align-middle"><del><strong>${{$offer->price}}</strong></del><strong
+            class="cl-success pl-2">${{$offer->discount_price()}}</strong></td>
     @else
-    <td class="text-center align-middle"><strong>${{$offer->price}}</strong></td>
+        <td class="text-center align-middle"><strong>${{$offer->price}}</strong></td>
     @endif
     <td class="text-center align-middle">
         <div class="btn-group-justified">
             @if(@$user != null)
             <button id="add_offer_to_cart_{{$offer->id}}"
                 onclick="pressed_add_offer_to_cart({{$offer->id}})" class="btn btn-orange"
-                {{ $user->banned() ? 'disabled' : ''}}><i class="fas fa-cart-plus"></i></button>
-            <!-- para adicionar a cookie, perguntar ao ruben como fez isto -->
+                {{ $user->banned() ? 'disabled' : ''}}><i class="fas fa-cart-plus"></i>
+            </button>
             @else
                 <button id="add_offer_to_cart_{{$offer->id}}"
-                    onclick="pressed_add_offer_to_cart({{$offer->id}})" class="btn btn-orange"><i class="fas fa-cart-plus"></i></button>
+                    onclick="pressed_add_offer_to_cart({{$offer->id}})" class="btn btn-orange"><i class="fas fa-cart-plus"></i>
+                </button>
             @endif
         </div>
     </td>
