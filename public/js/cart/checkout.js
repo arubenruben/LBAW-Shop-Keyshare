38b94c41768f3a7c6f5d1faafb3ee00385b53361
lockdownpt <guerra70@live.com.pt>
2020-05-09 13:05:21 +0100

    Passing to paypal verification, and how the process is made.

--- public/js/cart/checkout.js
+++ public/js/cart/checkout.js
@@ -1,7 +1,11 @@
 const checkout_tab_1 = document.querySelector("#checkout-tab-1");
 const checkout_tab_2 = document.querySelector("#checkout-tab-2");
+
 const comfirm_order_button = document.querySelector("#confirm-order");
 const your_info_button = document.querySelector("#your-info");
+
+const paypal_button = document.querySelector("#paypalButton");
+
 
 const input_name = document.querySelector("#checkoutInputName");
 const input_email = document.querySelector("#checkoutInputEmail");
@@ -19,6 +23,13 @@ const address_invalid = document.querySelector("#address-invalid");
 const zip_code_invalid = document.querySelector("#zip-code-invalid");
 
 let valid_inputs;
+
+const addEventListeners = () => {
+    comfirm_order_button.addEventListener('click', clicked_confirm_button);
+    your_info_button.addEventListener('click', clicked_info_button);
+    paypal_button.addEventListener('click', sendRequest);
+}
+
 
 const verify_input = (input_variable, invalid_block, invalid_text, extra_validation) => {
 
@@ -59,6 +70,17 @@ const clicked_confirm_button = () => {
 
 }
 
+const assembleData = () => {
+
+    let data = {};
+
+    data.name = client_name.innerHTML;
+    data.email = client_email.innerHTML;
+    data.address = client_address.innerHTML;
+    data.zip_code = client_zip_code.innerHTML;
+
+    return data;
+}
 
 const clicked_info_button = () => {
 
@@ -66,9 +88,62 @@ const clicked_info_button = () => {
     checkout_tab_2.style.display = "none";
 }
 
+const validate_info = (res) => {
 
-comfirm_order_button.addEventListener('click', clicked_confirm_button);
-your_info_button.addEventListener('click', clicked_info_button);
 
+
+}
+
+const sendRequest = () => {
+    let data = assembleData();
+    sendGet(data)
+        .then(res => validate_info(res))
+        .catch(error => console.error("Error: " + error));
+}
+
+const sendPut = post => {
+    const options = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*",
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRF-TOKEN": token
+        },
+        method: 'put',
+        credentials: "same-origin",
+        body: JSON.stringify(post)
+    }
+
+    return fetch("/cart/checkout", options)
+        .then(res => res.json())
+        .catch(error => console.error("Error: {error}"));
+}
+
+const sendGet = get => {
+    const options = {
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json, text-plain, */*",
+            "X-Requested-With": "XMLHttpRequest",
+            "X-CSRF-TOKEN": token
+        },
+        method: 'get',
+        credentials: "same-origin",
+    }
+
+    return fetch("api/validateCheckoutInfo?" + encodeForAjax(get), options)
+        .then(res => res.json())
+        .catch(error => console.error("Error: " + error));
+}
+
+
+function encodeForAjax(data) {
+    if (data == null) return null;
+    return Object.keys(data).map(function(k){
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&');
+}
+
+addEventListeners();
 
 
--- resources/views/partials/cart/checkoutTab2.blade.php
+++ resources/views/partials/cart/checkoutTab2.blade.php
@@ -65,6 +65,6 @@
     <hr>
     <div id="checkoutButtonsContainer" class="row">
         <a  id="your-info"  class="btn btn-blue btn-lg mr-auto ml-4"> <i class="fas fa-arrow-left"></i> <span class="d-none d-md-inline">Your Info</span></a>
-        <a id="paypalButton" href="checkout.php?page=3" role="button" class="btn btn-lg px-4  btn-outline-primary"> <img src="/pictures/paypal/paypalLogo.png" height="25"> <strong class="mr-2 d-none d-sm-inline-block" style="color: black; ">Pay with Paypal</strong> </a>
+        <a id="paypalButton" role="button" class="btn btn-lg px-4  btn-outline-primary"> <img src="/pictures/paypal/paypalLogo.png" height="25"> <strong class="mr-2 d-none d-sm-inline-block" style="color: black; ">Pay with Paypal</strong> </a>
     </div>
 </div>
