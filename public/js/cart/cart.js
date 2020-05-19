1d4eca91a837a1592011b8121d33b9ea3939f3df
Ruben Almeida <almeidaruben35@gmail.com>
2020-05-19 17:37:43 +0100

    Fixed cart price on remove

--- public/js/cart/cart.js
+++ public/js/cart/cart.js
@@ -1,36 +1,27 @@
 'use strict'
 
 const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-const cartItemCounter=document.querySelector("#shopping_cart_item_counter");
-const counter_products_cart=document.querySelector("#counter_products_cart");
+const cartItemCounter = document.querySelector("#shopping_cart_item_counter");
+const counter_products_cart = document.querySelector("#counter_products_cart");
 const url = '/cart';
 const totalPrice = document.querySelector("#total_price");
 let totalPriceNumber = 0;
 
 const addEventListeners = () => {
 
-    let buttons=document.querySelectorAll(".remove_cart_button");
+    let buttons = document.querySelectorAll(".remove_cart_button");
 
     buttons.forEach(button => {
-        button.addEventListener('click',()=>{
-            const cartId=button.getAttribute('data_cart_id')
-            const offerPrice=parseFloat(button.getAttribute('value_offer')).toFixed(2);
+        button.addEventListener('click', () => {
             event.preventDefault();
-            let totalPriceNumber = parseFloat(totalPrice.innerHTML).toFixed(2);
-            totalPriceNumber -= offerPrice;
-            totalPriceNumber = totalPriceNumber.toFixed(2);
-            totalPrice.innerHTML = totalPriceNumber + "$";
-            sendDelete(cartId).then(function() {
-                let tableEntry=document.querySelector('#row'+cartId);
-                tableEntry.remove();        
-                cartItemCounter.innerHTML=cartItemCounter.innerHTML-1;
-                counter_products_cart.innerHTML=counter_products_cart.innerHTML-1;
-              });              
+            const cartId = button.getAttribute('data_cart_id')
+            const offerPrice = parseFloat(button.getAttribute('value_offer')).toFixed(2);
+            sendDelete(cartId, offerPrice)
         });
     });
-   
+
 }
-const sendDelete = cartId => {
+const sendDelete = (cartId, offerPrice) => {
     const options = {
         method: 'delete',
         headers: new Headers({
@@ -40,9 +31,21 @@ const sendDelete = cartId => {
             "X-CSRF-TOKEN": token
         })
     }
-    return fetch(url+'/'+cartId, options)
-        .then(res => res.json())
-        .catch(error => console.error("Error:"+error));
+    return fetch(url + '/' + cartId, options)
+        .then(function (res) {
+            res.json();
+            if (res.ok) {
+                let totalPriceNumber = parseFloat(totalPrice.innerHTML).toFixed(2);
+                totalPriceNumber -= offerPrice
+                totalPrice.innerHTML = totalPriceNumber.toFixed(2);
+                let tableEntry = document.querySelector('#row' + cartId);
+                tableEntry.remove();
+                cartItemCounter.innerHTML = cartItemCounter.innerHTML - 1;
+                counter_products_cart.innerHTML = counter_products_cart.innerHTML - 1;
+            }
+
+        })
+        .catch(error => console.error("Error:" + error));
 }
 
 addEventListeners();
